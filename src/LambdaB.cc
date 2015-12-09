796200c2a35c5fb77d382a0863686d070168c707
nsahoo <niladribihari.sahoo@cern.ch>
2015-12-09 13:18:45 +0100

    minor update

--- src/LambdaB.cc
+++ src/LambdaB.cc
@@ -10,9 +10,11 @@
  Implementation:
      [Notes on implementation]
 */
-//
+//--------------------------------------------------------------------
+// >>> Ntuplizer code for /\b -> /\(->p+pi) mu+ mu- <<<
 // Original Author:  Niladribihari Sahoo,42 3-024,+41227662373,
 //         Created:  Tue Dec  8 23:43:26 CET 2015
+//--------------------------------------------------------------------
 // $Id$
 //
 //
@@ -22,13 +24,108 @@
 #include <memory>
 
 // user include files
+
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
-
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
-
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Common/interface/TriggerNames.h"
+
+#include "MagneticField/Engine/interface/MagneticField.h"
+#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
+
+#include <TFile.h>
+#include <TTree.h>
+#include <TMath.h>
+#include <TLorentzVector.h>
+#include <TH1.h>
+
+
+using namespace std;
+
+const int MUONMINUS_PDG_ID = 13;
+const int PIONPLUS_PDG_ID = 211;
+const int PROTON_PDG_ID = 2212;
+const int LAMBDA_PDG_ID = 3122;
+const int LAMBDAB_PDG_ID = 5122;
+const int JPSI_PDG_ID = 443;
+const int PSI2S_PDG_ID = 100443;
+
+const double PI = 3.141592653589793;
+
+// Structures                                                                                                                                                           
+struct HistArgs{
+  char name[128];
+  char title[128];
+  int n_bins;
+  double x_min;
+  double x_max;
+};
+
+enum HistName{
+  h_events,
+  h_mupt,
+  h_mueta,
+  h_mumdcabs,
+  h_mumutrkr,
+  h_mumutrkz,
+
+  h_mumudca,
+  h_mumuvtxcl,
+  h_mumupt,
+  h_mumumass,
+  h_mumulxybs,
+
+  h_mumucosalphabs,
+  h_trkpt,
+  h_trkdcasigbs,
+  h_lbvtxchisq,
+  h_lbvtxcl,
+
+  h_lzmass,   // lb = LambdaB, lz = Lambda
+  h_lbmass,
+
+  kHistNameSize
+};
+
+// Global hist args                                                                                                                                    
+
+HistArgs hist_args[kHistNameSize] = {
+  // name, title, n_bins, x_min, x_max                                                                                                                       
+
+  {"h_events", "Processed Events", 1, 0, 1},
+  {"h_mupt", "Muon pT; [GeV]", 100, 0, 30},
+  {"h_mueta", "Muon eta", 100, 0, 3},
+  {"h_mumdcabs", "#mu^{-} DCA beam spot; DCA [cm]", 100, 0, 10},
+  {"h_mumutrkr", "#mu^{+}#mu^{-} distance in phi-eta; [cm]", 100, 0, 50},
+  {"h_mumutrkz", "#mu^{+}#mu^{-} distance in Z; [cm]", 100, 0, 100},
+
+  {"h_mumudca",  "#mu^{+}#mu^{-} DCA; [cm]", 100, 0, 20},
+  {"h_mumuvtxcl",  "#mu^{+}#mu^{-} vertex CL", 100, 0, 1},
+  {"h_mumupt",    "#mu^{+}#mu^{-} pT ; pT [GeV]", 100, 0, 50},
+  {"h_mumumass", "#mu^{+}#mu^{-} invariant mass; M(#mu^{+}#mu^{-}) [GeV/c^{2}]",
+   100, 2, 20},
+  {"h_mumulxybs", "#mu^{+}#mu^{-} Lxy #sigma beam spot", 100, 0, 100},
+
+  {"h_mumucosalphabs", "#mu^{+}#mu^{-} cos #alpha beam spot", 100, 0, 1},
+  {"h_trkpt", "Pion track pT; pT [GeV]", 100, 0, 20},
+  {"h_trkdcasigbs", "Pion track DCA/#sigma beam spot; DCA/#sigma", 1000, 0, 100},
+  {"h_lbvtxchisq", "#lambda_{b} decay vertex chisq", 100, 0, 1000},
+  {"h_lbvtxcl", "#lambda_{b} decay vertex CL", 100, 0, 1},
+
+  {"h_lzmass", "#lambda^{0} mass; M(#lambda^{0}) [GeV/^{2}]", 100, 0, 20},
+
+  {"h_lbmass", "#lambda_{b} mass; M(#lambda_{b}) [GeV]", 100, 0, 20},
+
+};
+
+// Define histograms                                                                                                                                             
+TH1F *histos[kHistNameSize];
+
+
 //
 // class declaration
 //
@@ -50,6 +147,28 @@ class LambdaB : public edm::EDAnalyzer {
       virtual void endRun(edm::Run const&, edm::EventSetup const&);
       virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
       virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
+
+  bool buildLbToLzMuMu(const edm::Event &);
+
+  void calLS (double, double, double, double, double, double, double,
+              double, double,  double, double, double, double, double,
+              double, double, double, double, double*, double*);
+
+  void calCosAlpha (double, double, double, double, double,
+                    double, double, double, double, double,
+                    double, double, double, double,
+                    double, double, double, double,
+                    double*, double*);
+
+  void calCosAlpha2d (double, double, double, double, double,     /* added */
+                      double, double, double, double, double,
+                      double, double, double, double,
+                      double, double, double, double,
+                      double*, double*);
+
+
+
+
 
       // ----------member data ---------------------------
 };
@@ -90,19 +209,8 @@ LambdaB::~LambdaB()
 void
 LambdaB::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-   using namespace edm;
 
 
-
-#ifdef THIS_IS_AN_EVENT_EXAMPLE
-   Handle<ExampleData> pIn;
-   iEvent.getByLabel("example",pIn);
-#endif
-   
-#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
-   ESHandle<SetupData> pSetup;
-   iSetup.get<SetupRecord>().get(pSetup);
-#endif
 }
 
 
