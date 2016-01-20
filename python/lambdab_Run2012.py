0f2e1b8dcf87c139f270efa375b9cb7eeac51aba
nsahoo <niladribihari.sahoo@cern.ch>
2016-01-20 15:08:48 +0100

    added the config file to run over 2012 data

--- python/lambdab_Run2012.py
+++ python/lambdab_Run2012.py
@@ -1,20 +1,44 @@
+
+print "running on 2012 data \n"
+
 import FWCore.ParameterSet.Config as cms
-
-process = cms.Process("Demo")
-
-process.load("FWCore.MessageService.MessageLogger_cfi")
+from lambdab_cfi import process 
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
-    # replace 'myfile.root' with the source file you want to use
-    fileNames = cms.untracked.vstring(
-        'file:myfile.root'
+        fileNames = cms.untracked.vstring(
+'/store/data/Run2012D/MuOniaParked/AOD/22Jan2013-v1/10000/0009C032-C48D-E211-83FA-003048FEB956.root' )
     )
-)
+process.GlobalTag.globaltag = cms.string('FT53_V21A_AN6::All')
 
-process.demo = cms.EDAnalyzer('LambdaB'
-)
+# do trigger matching for muons
+triggerProcessName = 'HLT'
 
+process.cleanMuonTriggerMatchHLT0 = cms.EDProducer(
+    # match by DeltaR only (best match by DeltaR)
+    'PATTriggerMatcherDRLessByR',                         
+    src                   = cms.InputTag('cleanPatMuons'),
+    # default producer label as defined in
+    # PhysicsTools/PatAlgos/python/triggerLayer1/triggerProducer_cfi.py
+    matched               = cms.InputTag('patTrigger'),
+    matchedCuts           = cms.string('path("HLT_DoubleMu3p5_LowMass_Displaced*",0,0)'),
+    maxDeltaR             = cms.double(0.1),
+    # only one match per trigger object
+    resolveAmbiguities    = cms.bool(True),
+    # take best match found per reco object (by DeltaR here, see above)       
+    resolveByMatchQuality = cms.bool(False))
 
-process.p = cms.Path(process.demo)
+from PhysicsTools.PatAlgos.tools.trigTools import *
+switchOnTriggerMatchEmbedding(process, triggerMatchers = ['cleanMuonTriggerMatchHLT0'],
+                              hltProcess = triggerProcessName, outputModule = '')
+
+g_TriggerNames_LastFilterNames = [
+    ('HLT_DoubleMu3p5_LowMass_Displaced',  'hltDisplacedmumuFilterDoubleMu3p5LowMass') 
+    ]
+
+g_TriggerNames = [i[0] for i in g_TriggerNames_LastFilterNames]
+g_LastFilterNames = [i[1] for i in g_TriggerNames_LastFilterNames]
+
+process.ntuple.TriggerNames = cms.vstring(g_TriggerNames)
+process.ntuple.LastFilterNames = cms.vstring(g_LastFilterNames)
